825717ae995fd42c58c8ebcf38fe81d41686505f
Arnoldo Solis <arnoldo.solis62@myhunter.cuny.edu>
2021-04-17 16:01:04 -0400

    Implemented GenerateSortedVector

--- test_sorting_algorithms.cc
+++ test_sorting_algorithms.cc
@@ -46,45 +46,94 @@ vector<int> GenerateRandomVector(size_t size_of_vector)
 // Generate and returns sorted vector of size @size_of_vector
 // If smaller_to_larger is true, returns vector sorted from small to large
 // Otherwise returns vector sorted from large to small
-vector<int> GenerateSortedVector(size_t size_of_vector, bool smaller_to_larger) {
-  // ADD CODE
+vector<int> GenerateSortedVector(size_t size_of_vector, bool smaller_to_larger) 
+{
+  // Creates vector a
+  vector<int> a{};
+  // If the values are going to be from smallest to largest
+  if (smaller_to_larger == true)
+  {
+    // array will start from 1
+    size_t num = 1;
+    // Scans through vector
+    for (size_t i = 0; i < size_of_vector; i++)
+    {
+      // Inserts num
+      a.push_back(num);
+      // Will increase num to order from smallest to largest
+      num ++;
+    }
+    return a;
+  }
+  // If the values are going to be from largest to smallest
+  else if (smaller_to_larger == false)
+  {
+    // arry starts from its size
+    size_t num = size_of_vector;
+    for (size_t i = 0; i < size_of_vector; i++)
+    {
+      // Inserts num
+      a.push_back(num);
+      // Will decrease num to order from largest to smallest
+      num--;
+    }
+    return a;
+  }
+  // Empty
+  else 
+  {
+    return a;
+  }
 }
 
 // Verifies that a vector is sorted given a comparator
 template <typename Comparable, typename Comparator>
 bool VerifyOrder(const vector<Comparable> &input, Comparator less_than) 
 {
+  // Flag to check for verification of order
   bool verified = false;
+  // If comparator is less
   if (less_than == less<int>{})
   {
+    // Scan through input
     for (int i = 0; i < input.size() - 1; i++)
     {
+      // Check that vector is sorted from smallest to largest
       //less -> 1, 2, 3, 4, 5
       if (input[i] < input[i+1])
       {
+        // Will set verified to true if input at index is less than input at index + 1
         verified = true;
       }
       else
       {
+        // Will set verified to false if input at index is NOT less than input at index + 1
         verified = false;
       }
     }
+    // returns true if order ranges from smallest to largest
     return verified;
   }
+  // If comparator is greater
   else if(less_than == greater<int>{})
   {
+    // Scan through input
     for (int i = 0; i < input.size() - 1; i++)
     {
+      // Checks that the vector is sorted from largest to smallest
       // greater -> 5, 4, 3, 2, 1
       if (input[i] > input[i+1])
       {
+        // Will set verified to true if input at index is greater than input at index + 1
         verified = true;
       }
       else
       {
+        // Will set verified to false if input at index is NOT greater than input at index + 1
         verified = false;
       }
     }
+    // returns true if order ranges from largest to smallest
     return verified;
   }
 }
@@ -92,7 +141,6 @@ bool VerifyOrder(const vector<Comparable> &input, Comparator less_than)
 // Computes duration given a start time and a stop time in nano seconds
 long long ComputeDuration(chrono::high_resolution_clock::time_point start_time, chrono::high_resolution_clock::time_point end_time) {
   // Add code
-  
 }
 
 // Wrapper function to test the different sorting algorithms
